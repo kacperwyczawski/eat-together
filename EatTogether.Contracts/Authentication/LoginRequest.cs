341c8c6df007260f606f027fe30ba1973e75718f
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-02 19:40:17 +0100

    Add auth controller

---
+++ EatTogether.Contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace EatTogether.Contracts.Authentication;
+
+public record LoginRequest(string Email, string Password);
---
+++ EatTogether.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using EatTogether.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EatTogether.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Login(LoginRequest request)
+    {
+        return Ok();
+    }
+    
+    [HttpPost]
+    public IActionResult Register(RegisterRequest request)
+    {
+        return Ok();
+    }
+}
