a715aec5c2f879163b5de99e24ca234103e31564
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-10 20:36:45 +0100

    Add validation using FluentValidation

---
+++ EatTogether.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,40 @@
+﻿using EatTogether.Application.Authentication.Commands.Register;
+using EatTogether.Application.Authentication.Common;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace EatTogether.Application.Common.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationBehavior(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if(_validator is null)
+            return await next();
+        
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        
+        if (validationResult.IsValid)
+            return await next();
+
+        var errors = validationResult.Errors
+            .Select(error => Error.Validation(
+                description: error.ErrorMessage))
+            .ToList();
+
+        return (dynamic)errors; // compiler doesn't know that there is explicit conversion from List<Error> to IErrorOr
+    }
+}
--- EatTogether.WebApi/Program.cs
+++ EatTogether.WebApi/Program.cs
@@ -1,7 +1,12 @@
+using EatTogether.Application.Authentication.Commands.Register;
+using EatTogether.Application.Authentication.Common;
+using EatTogether.Application.Common.Behaviors;
 using EatTogether.Application.Common.Interfaces.Authentication;
 using EatTogether.Application.Common.Interfaces.Persistence;
 using EatTogether.Infrastructure.Authentication;
 using EatTogether.Infrastructure.Persistent;
+using ErrorOr;
+using FluentValidation;
 using Mapster;
 using MediatR;
 using MapsterMapper;
@@ -18,6 +23,8 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IMapper, Mapper>();
 builder.Services.AddSingleton(TypeAdapterConfig.GlobalSettings.Scan(AppDomain.CurrentDomain.GetAssemblies()));
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+builder.Services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
