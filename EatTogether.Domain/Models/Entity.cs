ac5c3027c96d66ab0aaa1bd77e1ae22e67a867dc
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-17 11:51:58 +0100

    Add AggregateRoot, Entity and ValueObject base classes

---
+++ EatTogether.Domain/Models/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace EatTogether.Domain.Models;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId : notnull
+{
+    public TId Id { get; protected set; }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public bool Equals(Entity<TId>? other) => Equals((object?)other);
+
+    public override bool Equals(object? obj) => obj is Entity<TId> entity && Id.Equals(entity.Id);
+
+    public static bool operator ==(Entity<TId> left, Entity<TId> right) => Equals(left, right);
+
+    public static bool operator !=(Entity<TId> left, Entity<TId> right) => !Equals(left, right);
+    
+    public override int GetHashCode() => Id.GetHashCode();
+}
---
+++ EatTogether.Domain/Models/ValueObject.cs
@@ -0,0 +1,47 @@
+﻿namespace EatTogether.Domain.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public bool Equals(ValueObject? other) => Equals((object?)other);
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+
+        var valueObject = (ValueObject)obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public override int GetHashCode() =>
+        GetEqualityComponents()
+            .Select(obj => obj?.GetHashCode() ?? 0)
+            .Aggregate((a, b) => a ^ b);
+
+    public static bool operator ==(ValueObject? left, ValueObject? right) => Equals(left, right);
+
+    public static bool operator !=(ValueObject? left, ValueObject? right) => !Equals(left, right);
+}
+
+public class Price : ValueObject
+{
+    public decimal Amount { get; private set; }
+    public string Currency { get; private set; }
+
+    public Price(decimal amount, string currency)
+    {
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Amount;
+        yield return Currency;
+    }
+}
