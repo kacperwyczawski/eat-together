149d2a3ab8a3860a162b71b693476eaded0961a5
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-18 12:56:30 +0100

    Remove price from ValueObject.cs

--- EatTogether.Domain/Seedwork/ValueObject.cs
+++ EatTogether.Domain/Seedwork/ValueObject.cs
@@ -27,21 +27,3 @@ public abstract class ValueObject : IEquatable<ValueObject>
 
     public static bool operator !=(ValueObject? left, ValueObject? right) => !Equals(left, right);
 }
-
-public class Price : ValueObject
-{
-    public decimal Amount { get; private set; }
-    public string Currency { get; private set; }
-
-    public Price(decimal amount, string currency)
-    {
-        Amount = amount;
-        Currency = currency;
-    }
-
-    public override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return Amount;
-        yield return Currency;
-    }
-}
