c09307bff40fedee14ba17e8ed5604bb2beb56fd
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-18 12:59:49 +0100

    Use rating class in menu

--- EatTogether.Domain/Features/Menu/Menu.cs
+++ EatTogether.Domain/Features/Menu/Menu.cs
@@ -1,18 +1,23 @@
 ﻿using EatTogether.Domain.Features.Menu.Entities;
+using EatTogether.Domain.Shared.ValueObjects;
 
 namespace EatTogether.Domain.Features.Menu;
 
 public sealed class Menu : AggregateRoot
 {
     public string Name { get; }
+    
     public string Description { get; }
-    public float AverageRating { get; }
+    
+    public Rating AverageRating { get; }
+    
     private readonly List<MenuSection> _sections = new();
 
-    public Menu(Guid id, string name, string description) : base(id)
+    public Menu(Guid id, string name, string description, Rating averageRating) : base(id)
     {
         Name = name;
         Description = description;
+        AverageRating = averageRating;
     }
     
     public IReadOnlyList<MenuSection> Sections => _sections.AsReadOnly();
