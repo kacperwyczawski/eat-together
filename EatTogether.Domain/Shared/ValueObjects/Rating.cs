a7b8cf21333b14fe1a65e1f7a16a8f10a9f706f4
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-18 13:22:39 +0100

    Add static average method to Rating.cs

--- EatTogether.Domain/Shared/ValueObjects/Rating.cs
+++ EatTogether.Domain/Shared/ValueObjects/Rating.cs
@@ -13,13 +13,13 @@ public class Rating : ValueObject
     }
 
     public static Rating FromFloat(float value) => new Rating(value);
-    
+
     public static Rating FromInt(int value) => new Rating(value);
-    
+
     public static implicit operator float(Rating rating) => rating._value;
-    
+
     public static implicit operator int(Rating rating) => (int)rating._value;
-    
+
     /// <example>4.5/5</example>
     public override string ToString() => $"{_value:0.0}/5";
 
@@ -27,4 +27,6 @@ public class Rating : ValueObject
     {
         yield return Math.Round(_value, 1);
     }
+
+    public static Rating Average(IEnumerable<Rating> ratings) => Rating.FromFloat(ratings.Average(r => (float)r));
 }
