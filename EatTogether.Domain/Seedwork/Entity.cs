1a086ee60dd58d5feb636263f6db6d663f3e518a
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-18 12:22:15 +0100

    Use Guid as id in entity base class

--- EatTogether.Domain/Seedwork/Entity.cs
+++ EatTogether.Domain/Seedwork/Entity.cs
@@ -1,22 +1,18 @@
 ﻿namespace EatTogether.Domain.Seedwork;
 
-public abstract class Entity<TId> : IEquatable<Entity<TId>>
-    where TId : notnull
+public abstract class Entity : IEquatable<Entity>
 {
-    public TId Id { get; protected set; }
+    public Guid Id { get; protected set; }
 
-    protected Entity(TId id)
-    {
-        Id = id;
-    }
+    protected Entity(Guid id) => Id = id;
 
-    public bool Equals(Entity<TId>? other) => Equals((object?)other);
+    public bool Equals(Entity? other) => Equals((object?)other);
 
-    public override bool Equals(object? obj) => obj is Entity<TId> entity && Id.Equals(entity.Id);
+    public override bool Equals(object? obj) => obj is Entity entity && Id.Equals(entity.Id);
 
-    public static bool operator ==(Entity<TId> left, Entity<TId> right) => Equals(left, right);
+    public static bool operator ==(Entity left, Entity right) => Equals(left, right);
 
-    public static bool operator !=(Entity<TId> left, Entity<TId> right) => !Equals(left, right);
+    public static bool operator !=(Entity left, Entity right) => !Equals(left, right);
     
     public override int GetHashCode() => Id.GetHashCode();
 }
