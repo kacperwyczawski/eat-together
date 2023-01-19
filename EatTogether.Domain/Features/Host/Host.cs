f79ec4898300bbaf46462bba0b3be5066c09a10f
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-19 12:54:42 +0100

    Add host feature to domain

---
+++ EatTogether.Domain/Features/Host/Host.cs
@@ -0,0 +1,15 @@
+﻿namespace EatTogether.Domain.Features.Host;
+
+public sealed class Host : AggregateRoot
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string ProfilePictureUrl { get; }
+
+    public Host(Guid id, string firstName, string lastName, string profilePictureUrl) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        ProfilePictureUrl = profilePictureUrl;
+    }
+}
