56f35d298ed7a3d3f2baca347082e0588b657270
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-19 12:59:28 +0100

    Add user feature to domain

---
+++ EatTogether.Domain/Features/User/User.cs
@@ -0,0 +1,17 @@
+﻿namespace EatTogether.Domain.Features.User;
+
+public sealed class User : AggregateRoot
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public string Email { get; }
+    public string Password { get; } // TODO: Hash password
+
+    public User(Guid id, string firstName, string lastName, string email, string password) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+}
