c8c4125c11c65c34af66c3e3bbea042941704fed
Kacper Wyczawski <wyczawskikacper@gmail.com>
2023-01-19 12:57:11 +0100

    Add menu review feature to domain

---
+++ EatTogether.Domain/Features/MenuReview/MenuReview.cs
@@ -0,0 +1,15 @@
+﻿using EatTogether.Domain.Shared.ValueObjects;
+
+namespace EatTogether.Domain.Features.MenuReview;
+
+public sealed class MenuReview : AggregateRoot
+{
+    public Rating Rating { get; }
+    public string Comment { get; }
+
+    public MenuReview(Guid id, Rating rating, string comment) : base(id)
+    {
+        Rating = rating;
+        Comment = comment;
+    }
+}
